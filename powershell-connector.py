8f7010cf9d63a7774fcd9bfd4bbbdf6d576d4884
Leonardo Miranda Gomes <l.mirandagomes.inf25@stud.bbbaden.ch>
2026-06-20 14:46:24 +0200

    Made authentication work through a decorator instead of copying and pasting the same piece of code

--- powershell-connector.py
+++ powershell-connector.py
@@ -1,18 +1,46 @@
+import enum
+import json
+from dataclasses import asdict
+from functools import wraps
+from sys import argv
+from requests import Session
+
 from MoodleDownloader.MoodleDownloader import (
     MoodleAuth,
     MoodleCredentials,
     MoodleSession,
     MoodleDatabase,
     Scraper
-    )
-from sys import argv
-from requests import Session
+)
 
 
-def arg_session_valid(*var):
-    moodle_session = MoodleSession(login_cookies={"MoodleSession": var[0]})
-    auth = MoodleAuth(Session(), moodle_session=moodle_session)
+class MoodleJsonEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, enum.Enum):
+            return obj.value
+        return super().default(obj)
+
+
+def authenticated(func):
+    @wraps(func)
+    def wrapper(*var):
+        moodle_session = MoodleSession(login_cookies={"MoodleSession": var[0]})
+        auth = MoodleAuth(Session(), moodle_session=moodle_session)
+        return func(auth, *var[1:])
+    return wrapper
+
+
+@authenticated
+def arg_session_valid(auth):
     return auth.is_session_valid()
+
+
+@authenticated
+def arg_course(auth, course_id):
+    scraper = Scraper(auth.session, auth)
+    course = scraper.create_dataclass(int(course_id))
+    return json.dumps(asdict(course), cls=MoodleJsonEncoder)
+
 
 def arg_login(*var):
     moodle_credentials = MoodleCredentials(var[0], var[1])
@@ -20,9 +48,11 @@ def arg_login(*var):
     moodle_session = moodle_auth.login()
     return moodle_session.login_cookies["MoodleSession"]
 
+
 actions = {
     "sessionvalid": arg_session_valid,
-    "login": arg_login
+    "login": arg_login,
+    "course": arg_course
 }
 
 
